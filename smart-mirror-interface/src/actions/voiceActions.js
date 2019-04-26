b35804b6741f4d0118a66917af79834dfded89b2
Awab Ijaz <Awab.Ijaz@gmail.com>
2019-04-27 01:33:36 +0500

    Latest updates

--- smart-mirror-interface/src/actions/voiceActions.js
+++ smart-mirror-interface/src/actions/voiceActions.js
@@ -1,41 +1,48 @@
-import { FETCH_VOICE, FETCH_WAKE_WORD, NEW_REMINDER, CHANGE_MESSAGE, FETCH_VIDEO_ID, STOP_VIDEO, FETCH_USER } from './types';
+import { FETCH_VOICE, FETCH_WAKE_WORD, NEW_REMINDER, NEW_ALARM, CHANGE_MESSAGE, FETCH_VIDEO_ID, STOP_VIDEO, FETCH_USER } from './types';
 import axios from 'axios';
 
+let message = '';
+
 export const fetchVoice = () => dispatch => {
-  axios.get('http://localhost:4000/exp')
+  axios.get('http://localhost:4000/commands')
       .then(response => {
         console.log(response.data);
+        if(response.data[0] === 'error'){
+          console.log('lmao');
+          message = 'Could not understand what you said, try again';
+        }
+        else if(response.data[1] === 'command not found'){
+          message = 'Command not found, try again. You said: ' + response.data[0];
+        }
+        else if(response.data[0] === 'Network error'){
+          message = 'Network error, check your internet connection';
+        }
+        else{
+          console.log('in command or in intent'); 
+          message = 'You said: ' + response.data[0];
+        }
         dispatch({
-            type: FETCH_VOICE,
-            payload1: 'You said: ' + response.data[0],
-            payload2: response.data[1],
-            payload3: response.data[2],
-            payload4: response.data[3],
-            payload5: false,
+          type: FETCH_VOICE,
+          payload1: message,
+          payload2: response.data[1],
+          payload3: response.data[2],
+          payload4: response.data[3],
+          payload5: false,
         })
       })
       .catch(function (error) {
         console.log(error);
       })  
-};
+}
 
 export const fetchWakeWord = () => dispatch => {
-  axios.get('http://localhost:4000/wakeWord')
-      .then(response => {
-        let yoloo = response.data.trim();
-        if(yoloo === "detected"){
-          console.log('wakeWord detected')
-          dispatch({
-            type: FETCH_WAKE_WORD,
-            payload1: true,
-            payload2: 'Listening...',
-            payload3: '',
-          })
-        }
-      })
-      .catch(function (error) {
-        console.log(error);
-      })  
+  dispatch({
+    type: FETCH_WAKE_WORD,
+    payload1: true,
+    payload2: 'Please wait',
+    payload3: '',
+    payload4: '',
+  })   
 }
 
 export const createReminder = (value,date,username) => dispatch => {
@@ -45,16 +52,29 @@ export const createReminder = (value,date,username) => dispatch => {
         console.log(result.data);
         dispatch({
             type: NEW_REMINDER,
-            payload1: result.data,
+            payload1: result.data+'. '+message,
             payload2: ''
         })
      });
 }
 
-export const changeMessage = username => dispatch => {
+export const setAlarm = (day,time,dayNumber,hours,minutes,username) => dispatch => {
+  axios.post('http://localhost:4000/users/db_alarms', 
+    { day:day, time:time, dayNumber:dayNumber, hours:hours, minutes:minutes, username:username })
+      .then((result) => {
+        console.log(result.data);
+        dispatch({
+            type: NEW_ALARM,
+            payload1: result.data+'. '+message,
+            payload2: ''
+        })
+     });
+}
+
+export const changeMessage = message => dispatch => {
   dispatch({
       type: CHANGE_MESSAGE,
-      payload: 'Hey, '+username,
+      payload: message,
   }) 
 }
 
