f0b50882f264a6c753af35dc4f49bef41809d6c5
Awab Ijaz <Awab.Ijaz@gmail.com>
2019-04-27 01:35:08 +0500

    Latest updates

--- smart-mirror-interface/server/routes/facial.js
+++ smart-mirror-interface/server/routes/facial.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+
+//idhar windows wala bana kar rakh de aka recognize_from_live_pic etc.
+//check index/wakeword for that example
 router.get('/recognize', function(req, res, next) {
-	console.log('inside face experiment');
+	console.log('inside facial server');
 	const spawn = require("child_process").spawn;
-	const pythonProcess = spawn('python',["C:\\Users\\AWAB\\Desktop\\face\\recognize_faces_image_pic.py", "-e", "C:\\Users\\AWAB\\Desktop\\face\\encodings.pickle"]);
+	var file = req.dest + "userFromPic_win.py";
+	console.log(file);
+	const pythonProcess = spawn('python',[file]);
 	pythonProcess.stdout.on('data', (data) => {
-    	res.send(data);
+		console.log(data);
+    	res.status(200).send(data);
 	});	
 });
-
 
 module.exports = router;
--- smart-mirror-interface/server/routes/python/finalDraft.py
+++ smart-mirror-interface/server/routes/python/finalDraft.py
@@ -1,8 +1,8 @@
-import speech_recognition as sr
 from os import path
 import sys
 import requests
 import json
+import speech_recognition as sr
 
 # Wit speech API endpoint
 API_ENDPOINT = 'https://api.wit.ai/message?v=20170307&q='
@@ -11,11 +11,14 @@ API_ENDPOINT = 'https://api.wit.ai/message?v=20170307&q='
 wit_access_token = '2C2LGRSCVA6MQ2K53XCPBWMD52A4XSLF'
 
 r = sr.Recognizer()
-
+#usb_mic = sr.Microphone.list_microphone_names().index("usb_mic")
+#mic = sr.Microphone(device_index=usb_mic)
 mic = sr.Microphone()
 with mic as source:
-    r.adjust_for_ambient_noise(source)
-    audio = r.listen(source)
+    r.adjust_for_ambient_noise(source,0.5)
+    #print('say')
+    audio = r.record(source, duration=5)
+    #print('over')
 
 def getIntent(text):
     # defining headers for HTTP request
@@ -23,30 +26,50 @@ def getIntent(text):
     # making an HTTP post request
     resp = requests.post(API_ENDPOINT + text, headers = headers)
     # converting response content to JSON format
-    data = json.loads(resp.content)
+    #strnobytesissue data = json.loads(resp.content)
+    data = json.loads(resp.content.decode('utf-8'))
     # get text from data
-    intent=data['entities']['intent'][0]['value']
-    print(intent)
-    if(intent == 'play video'):
-        if 'youtube_param' in data['entities']:
-            print(data['entities']['youtube_param'][0]['value'])
-        else:
+    
+    if 'intent' in data['entities']:
+        intent=data['entities']['intent'][0]['value']
+        print(intent)
+        if(intent == 'play video'):
+            if 'youtube_param' in data['entities']:
+                print(data['entities']['youtube_param'][0]['value'])
+            else:
+                print(data['entities']['reminder'][0]['value'])
+        elif(intent == 'expand'):
+            print(data['entities']['widget_name'][0]['value'])
+        elif(intent == 'create_reminder'):
             print(data['entities']['reminder'][0]['value'])
-    elif(intent == 'expand'):
-        print(data['entities']['widget_name'][0]['value'])
-    elif(intent == 'create_reminder'):
-        print(data['entities']['reminder'][0]['value'])
-        print(data['entities']['datetime'][0]['value'])
+            print(data['entities']['datetime'][0]['value'])
+        elif(intent == 'create_alarm'):
+            print(data['entities'])
+            print(data['entities']['datetime'][0]['value'])
+    else:
+        print('command not found')
 try:
     googleText = r.recognize_google(audio)
-    print(googleText)
-    getIntent(googleText)
+    if googleText == 'login' or googleText == 'log in' or googleText == 'signin' or googleText == 'sign in':
+        print(googleText)
+        print("command")
+        print("login")
+    elif googleText == 'logout' or googleText == 'log out' or googleText == 'signout' or googleText == 'sign out':
+        print(googleText)
+        print("command")
+        print("logout")
+    else:
+        print(googleText)
+        getIntent(googleText)
     sys.stdout.flush()
 except sr.UnknownValueError:
-    dataa = {
-        "text": "Google Speech Recognition could not understand audio"
-    }
-    print(dataa)
+    #dataa = {
+    #    "text": "Google Speech Recognition could not understand audio"
+    #}
+    #print(dataa)
+    print('error')
+    print('couldnt understand you')
 except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    #print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    print("Network error")
 
